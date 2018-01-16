6772fbc0fdaad18468c75f78380e44b892a905b3
gazvoda <>
2018-01-16 16:31:16 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="prijava.php" method="post">
+		Uporabnisko ime <input type="text" name="uporabnisko_ime"> 
+		Geslo <input type="password" name="geslo" />
+		<input type="submit" value="Prijava">
+	</form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
