4537d2a1437290967a6d15c5ed3b00ef0dee474d
gazvoda <ep@ep>
2018-01-17 15:25:47 +0100

    Admin log in and log out, https redirect, session

---
+++ prijava-stranka.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//$uporabnisko_ime = $_POST["uporabnisko_ime"];
+//$geslo = $_POST["geslo"];
+session_start();
+echo "Prijava uspesna za stranko";
+var_dump($_SESSION);
+
+
